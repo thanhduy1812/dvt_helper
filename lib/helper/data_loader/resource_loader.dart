e6b353a03aedde5721244117b3a19792c03c966a
duynt <duynt@flomail.net>
2024-05-12 20:54:55 +0700

    init package

---
+++ lib/helper/data_loader/resource_loader.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/services.dart';
+
+class DVTResourceLoader {
+  // static const String gotadiInvoicePath = "gotadi_data/gotadi_invoice_term.html";
+  static Future<String> loadContentFromResource({required String pathResource}) {
+    // final loader = rootBundle.loadString("packages/gtd_utils/lib/data/resource/$pathResource");
+    final loader = rootBundle.loadString(pathResource);
+    return loader;
+  }
+}
